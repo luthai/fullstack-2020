8b25c6a37dbd6f9478ffda1fbb2c0dbe66863546
luthai <luan086@gmail.com>
2020-05-19 20:24:02 +0200

    Finish unicafe 1.7

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -5,12 +5,22 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
 
-  const handleGoodClick = () => setGood(good + 1)
+  const handleGoodClick = () => {
+    setAll(all + 1)
+    setGood(good + 1)
+  }
+
+  const handleNeutralClick = () => {
+    setAll(all + 1)
+    setNeutral(neutral + 1)
+  }
   
-  const handleNeutralClick = () => setNeutral(neutral + 1)
-  
-  const handleBadClick = () => setBad(bad + 1)
+  const handleBadClick = () => {
+    setAll(all + 1)
+    setBad(bad + 1)
+  }
   
   return (
     <div>
@@ -22,6 +32,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {(good + (neutral * 0) + (bad * -1)) / all}</p>
+      <p>positive {(good / all) * 100}</p>
     </div>
   )
 }
