8e0378ae93ae64fe1e9fe5b068d48a833436712c
luthai <luan086@gmail.com>
2020-05-29 20:36:29 +0200

    Begin part2 finish course 2.1

---
+++ part2/course/src/index.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Header = (props) => 
+    <h1>{props.name}</h1>
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map(part =>
+        <p key={part.id}> 
+          {part.name} {part.exercises}
+        </p>
+      )}
+      </div>
+  )
+}
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+    </div>  
+  ) 
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
