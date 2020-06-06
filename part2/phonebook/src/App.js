752427db085ff027128f008e4c1d8384e7879cff
luthai <luan086@gmail.com>
2020-06-06 14:02:51 +0200

    Finish phonebook 2.6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -10,21 +10,40 @@ const App = () => {
   ])
   const [ newName, setNewName ] = useState('')
 
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName
+    }
+
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
+  
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input />
+          name: 
+          <input 
+            value={newName}
+            onChange={handleNameChange}
+          />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      <ul class="no-bullets">
+      <ul className="no-bullets">
         {persons.map(person => 
-          <Person person={person} />
+          <Person key={person.name} person={person} />
         )}
       </ul>
     </div>
