82f35e68d17008d8b92246b86719090f6846067f
luthai <luan086@gmail.com>
2020-07-02 18:28:26 +0200

    Finish phonebook 3.7

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,7 +1,11 @@
 const express = require('express')
+const morgan = require('morgan')
+
 const { request, response } = require('express')
 const app = express()
+
 app.use(express.json())
+app.use(morgan('tiny'))
 
 let persons = [
   {
