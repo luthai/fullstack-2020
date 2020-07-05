d5c5ab51b45df2a30a62b52547bd49ec6a47a3d8
luthai <luan086@gmail.com>
2020-07-05 18:40:01 +0200

    Phonebook 3.9

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const { request, response, json } = require('express')
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 morgan.token('body', function getBody(req) {
   if (Object.keys(req.body).length === 0 &&
