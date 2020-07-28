d647ee44b80c94fade4d592ce158a5db4258bc1d
luthai <luan086@gmail.com>
2020-07-28 20:53:25 +0200

    Blog-list 4.5

--- part4/blog_list/utils/list_helpers.js
+++ part4/blog_list/utils/list_helpers.js
@@ -4,7 +4,10 @@ const totalLikes = (blogs) => blogs
   .map((blog) => blog.likes)
   .reduce((total, item) => total + item);
 
+const favoriteBlog = (blogs) => blogs.reduce((acc, cur) => (cur.likes > acc.likes ? cur : acc));
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
