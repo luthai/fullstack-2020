5b5043de778801c739597a22739a3c552b86729e
luthai <luan086@gmail.com>
2020-07-28 18:48:11 +0200

    Blog-list 4.3

--- part4/blog_list/.eslintrc.js
+++ part4/blog_list/.eslintrc.js
@@ -1,4 +1,10 @@
 module.exports = {
+  env: {
+    commonjs: true,
+    es6: true,
+    node: true,
+    jest: true,
+  },
   extends: 'airbnb',
   rules: {
     'linebreak-style': ['error', 'windows'],
