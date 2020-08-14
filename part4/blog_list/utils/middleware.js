5bc9e44875f2482b3a332e47bc4cd0657e55c7ed
luthai <luan086@gmail.com>
2020-08-14 14:16:18 +0200

    Blog-list 4.20

--- part4/blog_list/utils/middleware.js
+++ part4/blog_list/utils/middleware.js
@@ -24,8 +24,18 @@ const errorHandler = (error, request, response, next) => {
   return next(error);
 };
 
+const tokenExtractor = (request, response, next) => {
+  const authorization = request.get('authorization');
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    request.token = authorization.substring(7);
+  }
+
+  next();
+};
+
 module.exports = {
   requestLogger,
   unknownEndpoint,
   errorHandler,
+  tokenExtractor,
 };
