6cc8352af342b31a6a26a91aa8ceb92debcbfaac
luthai <luan086@gmail.com>
2020-08-19 18:46:36 +0200

    Connecting frontend to backend

--- part4/blog_list/app.js
+++ part4/blog_list/app.js
@@ -29,6 +29,7 @@ app.use(cors());
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static('build'));
 app.use(middleware.requestLogger);
 app.use(middleware.tokenExtractor);
 
---
+++ part4/blog_list/build/precache-manifest.2471fdb60dc8b771db7b0f224b652abb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b4025c4fc11a129e11a7e2e2df37e11",
+    "url": "/index.html"
+  },
+  {
+    "revision": "38f0a78ccdbbb6d433e3",
+    "url": "/static/js/2.9c6d5b9b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9c6d5b9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89d8d567cf5c268066f2",
+    "url": "/static/js/main.827106d3.chunk.js"
+  },
+  {
+    "revision": "038c38d3471e45bc1ed6",
+    "url": "/static/js/runtime-main.63d0d90a.js"
+  }
+]);
