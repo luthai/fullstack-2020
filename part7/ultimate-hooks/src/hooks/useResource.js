1f1971d397cb8159b57a73859c3f5d45df504ff9
luthai <luan086@gmail.com>
2020-10-05 19:40:15 +0200

    ultimate-hooks 7.8

---
+++ part7/ultimate-hooks/src/hooks/useResource.js
@@ -0,0 +1,26 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+export const useResource = (baseUrl) => {
+  const [resources, setResources] = useState([])
+
+  useEffect(() => {
+    axios
+      .get(baseUrl)
+      .then(response => {
+        setResources(response.data)
+      })
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  const create = async (resource) => {
+    const response = await axios.post(baseUrl, resource)
+    setResources(resources.concat(response.data))
+  }
+
+  const service = {
+    create
+  }
+
+  return [resources, service]
+}
