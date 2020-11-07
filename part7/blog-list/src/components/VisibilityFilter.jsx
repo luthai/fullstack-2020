c304d5511624d87c33fe8faf5f952cf84202a811
luthai <luan086@gmail.com>
2020-11-07 12:10:00 +0100

    BlogList small bug

--- part7/blog-list/src/components/VisibilityFilter.jsx
+++ part7/blog-list/src/components/VisibilityFilter.jsx
@@ -13,42 +13,40 @@ import Users from './UsersView';
 import UserBlogList from './UserBlogList';
 import BlogsList from './BlogsList';
 
-const VisibilityFilter = (props) => {
-  return (
-    <div>
-      <Router>
-        {
-          props.user === null
-            ? (
+const VisibilityFilter = (props) => (
+  <div>
+    <Router>
+      {
+        props.user === null
+          ? (
+            <div>
+              <h2>Log in to application</h2>
+              <LoginForm />
+            </div>
+          )
+          : (
+            <div>
               <div>
-                <h2>Log in to application</h2>
-                <LoginForm />
+                <Link className="nav-link" to="/">home</Link>
+                <Link className="nav-link" to="/users">users</Link>
               </div>
-            )
-            : (
-              <div>
-                <div>
-                  <Link className="nav-link" to="/">home</Link>
-                  <Link className="nav-link" to="/users">users</Link>
-                </div>
-                <h2>Blogs</h2>
-                <br />
-                <p>{props.user.username} logged in</p>
-                <LogoutForm />
-                <br />
-                <Switch>
-                  <Route path="/users/:id" component={UserBlogList} />
-                  <Route path="/users" component={Users} />
-                  <Route path="/blogs/:id" component={BlogsList} />
-                  <Route path="/" component={Home} />
-                </Switch>
-              </div>
-            )
-        }
-      </Router>
-    </div>
-  );
-};
+              <h2>Blogs</h2>
+              <br />
+              <p>{props.user.username} logged in</p>
+              <LogoutForm />
+              <br />
+              <Switch>
+                <Route path="/users/:id" component={UserBlogList} />
+                <Route path="/users" component={Users} />
+                <Route path="/blogs/:id" component={BlogsList} />
+                <Route path="/" component={Home} />
+              </Switch>
+            </div>
+          )
+      }
+    </Router>
+  </div>
+);
 
 VisibilityFilter.propTypes = {
   user: PropTypes.objectOf(PropTypes.string),
