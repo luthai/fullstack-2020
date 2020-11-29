60ae8c7e00f7fcf831461b9ecc0ba1a7dd3df295
luthai <luan086@gmail.com>
2020-11-29 11:00:59 +0100

    library-frontend 8.8

--- part8/library-frontend/src/App.js
+++ part8/library-frontend/src/App.js
@@ -1,17 +1,37 @@
-import React from 'react'
-import { useQuery } from '@apollo/client'
-import { ALL_AUTHORS } from './queries'
+import React, { useState } from 'react'
+import Authors from './components/Authors'
+import Books from './components/Books'
+import NewBook from './components/NewBook'
 
 const App = () => {
-  const result = useQuery(ALL_AUTHORS)
-
-  if (result.loading) {
-    return <div>loading...</div>
-  }
-
+  const [page, setPage] = useState('authors')
+  /*
   return (
     <div>
       {result.data.allAuthors.map(a => a.name).join(', ')}
+    </div>
+  )*/
+
+  return (
+    <div>
+      <div>
+        <button onClick={() => setPage('authors')}>authors</button>
+        <button onClick={() => setPage('books')}>books</button>
+        <button onClick={() => setPage('add')}>add book</button>
+      </div>
+
+      <Authors
+        show={page === 'authors'}
+      />
+
+      <Books
+        show={page === 'books'}
+      />
+
+      <NewBook
+        show={page === 'add'}
+      />
+
     </div>
   )
 }
